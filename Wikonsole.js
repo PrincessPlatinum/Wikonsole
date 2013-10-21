60200b00c3defd2be3abbbb7c9d6b838ad924e14
Princess Platinum (Wikia Username) <princess.platinum.wikia@gmail.com>
2013-10-21 09:21:52 -0400

    Update Wikonsole.js

--- Wikonsole.js
+++ Wikonsole.js
@@ -1,28 +1,17 @@
-(function($) {
-    function moveCaretToEnd(el) {
-        if (typeof el.selectionStart == 'number') {
-            el.selectionStart = el.selectionEnd = el.value.length;
-        } else if (typeof el.createTextRange != 'undefined') {
-            el.focus();
-            var range = el.createTextRange();
-            range.collapse(false);
-            range.select();
+(function ($) {
+    var wc = {
+        css: '#console {border-radius: 0em;-moz-border-radius: 0em;-webkit-border-radius: 0em;border: 4px solid green;background: #000;color: green;font-family: "Lucida Console", Monaco, monospace;font-size: 12pt;font-weight: 700;margin-left: 5%;margin-right: 5%;width: 90%;height: 400px;}',
+        html: '<textarea type="text" id="console" wrap="soft">> </textarea>',
+        init: function () {
+            document.getElementById('WikiaPage').innerHTML = wc.html;
+            mw.util.addCSS(wc.css);
         }
-    }
-
-    var textarea = document.getElementById('console');
-
-    textarea.onfocus = function() {
-        moveCaretToEnd(textarea);
-        window.setTimeout(function() {
-            moveCaretToEnd(textarea);
-        }, 1);
     };
 
-    $('textarea#console').keypress(function(event) {
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        if(keycode == '13') {
-            processInput();
-        }
-    });
-}(jQuery));
+    //is the right page?
+    if (mw.config.get('wgPageName') != 'Special:Console') {
+        return;
+    } else {
+        wc.init;
+    }
+});
