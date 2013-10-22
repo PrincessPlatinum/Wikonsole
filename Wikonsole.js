b36e3f9e2ce52e54b9235c47f843621bfdfa3b54
Princess Platinum (Wikia Username) <princess.platinum.wikia@gmail.com>
2013-10-22 09:36:12 -0400

    Update Wikonsole.js

--- Wikonsole.js
+++ Wikonsole.js
@@ -1,10 +1,15 @@
 (function ($) {
     var wc = {
         init: function () {
-            $('.WikiaArticle').html('<div id="console-wrapper"><div id="console-output"><span class="output-line">> Welcome to Wikonsole!</span></div><input type="text" id="console-input" placeholder="Type Commands Here"/></div>');
+            $('.WikiaArticle').html('<div id="console-wrapper"><div id="console-output"></div><input type="text" id="console-input" placeholder="Type Commands Here"/></div>');
             mw.util.addCSS('#console-wrapper{font-family: "Lucida Console", Monaco, monospace;border: 5px solid green;background: black;width: 500px;text-align: center;}#console-output{font-family: "Lucida Console", Monaco, monospace;color: green;background: black;width:480px;height: 400px;text-align: left;padding: 4px 4px 4px 4px;overflow: auto;}#console-input{border-radius: 0em;-webkit-border-radius: 0em;-moz-border-radius: 0em;background: #000;color: green;font-family: "Lucida Console", Monaco, monospace;font-size: 12pt;width: 480px;}');
             $('.WikiaPageHeader').find('h1').text('Wikonsole');
             $('title').text('Wikonsole');
+            wc.print('Welcome to Wikonsole!');
+        },
+        print: function(message) {
+            var text = message.replace(/'/g, '\');
+            $('#console-output').append('> <span class="output-line">' + text + '</span>');
         }
     };
 
