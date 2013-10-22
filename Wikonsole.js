36fcda50fe842d1675b967cead4f4a704c65288c
Princess Platinum (Wikia Username) <princess.platinum.wikia@gmail.com>
2013-10-22 08:47:09 -0400

    Update Wikonsole.js

--- Wikonsole.js
+++ Wikonsole.js
@@ -1,8 +1,8 @@
 (function ($) {
     var wc = {
         init: function () {
-            $('.WikiaArticle').html('<textarea type="text" id="console" wrap="soft">> </textarea>');
-            mw.util.addCSS('#console {border-radius: 0em;-moz-border-radius: 0em;-webkit-border-radius: 0em;border: 4px solid green;background: #000;color: green;font-family: "Lucida Console", Monaco, monospace;font-size: 12pt;font-weight: 700;margin-left: 5%;margin-right: 5%;width: 90%;height: 400px;}');
+            $('.WikiaArticle').html('<div id="console-wrapper"><div id="console-output"><span class="output-line">> Welcome to Wikonsole!</span></div><input type="text" id="console-input" placeholder="Type Commands Here"/></div>');
+            mw.util.addCSS('#console-wrapper{font-family: "Lucida Console", Monaco, monospace;border: 5px solid green;background: black;width: 500px;text-align: center;}#console-output{font-family: "Lucida Console", Monaco, monospace;color: green;background: black;width:480px;height: 400px;text-align: left;padding: 4px 4px 4px 4px;overflow: auto;}#console-input{border-radius: 0em;-webkit-border-radius: 0em;-moz-border-radius: 0em;background: #000;color: green;font-family: "Lucida Console", Monaco, monospace;font-size: 12pt;width: 480px;}');
         }
     };
 
