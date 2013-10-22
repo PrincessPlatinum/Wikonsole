1231ad5348cb7fd28969deb00b1c3dde2f65fe93
Princess Platinum (Wikia Username) <princess.platinum.wikia@gmail.com>
2013-10-21 23:24:08 -0400

    Update Wikonsole.js

--- Wikonsole.js
+++ Wikonsole.js
@@ -1,10 +1,8 @@
 (function ($) {
     var wc = {
-        css: '#console {border-radius: 0em;-moz-border-radius: 0em;-webkit-border-radius: 0em;border: 4px solid green;background: #000;color: green;font-family: "Lucida Console", Monaco, monospace;font-size: 12pt;font-weight: 700;margin-left: 5%;margin-right: 5%;width: 90%;height: 400px;}',
-        html: '<textarea type="text" id="console" wrap="soft">> </textarea>',
         init: function () {
-            $('.WikiaArticle').html(wc.html);
-            mw.util.addCSS(wc.css);
+            $('.WikiaArticle').html('<textarea type="text" id="console" wrap="soft">> </textarea>');
+            mw.util.addCSS('#console {border-radius: 0em;-moz-border-radius: 0em;-webkit-border-radius: 0em;border: 4px solid green;background: #000;color: green;font-family: "Lucida Console", Monaco, monospace;font-size: 12pt;font-weight: 700;margin-left: 5%;margin-right: 5%;width: 90%;height: 400px;}');
         }
     };
 
